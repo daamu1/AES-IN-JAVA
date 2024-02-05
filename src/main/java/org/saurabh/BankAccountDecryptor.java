e3200be6bb9f647cda7976c0e0741fbe6e5c601e
Saurabh Maithani <saurabhmaithani01921@gmail.com>
2024-02-05 15:52:18 +0530

    corruct the naming convension

--- src/main/java/org/saurabh/BankAccountDecryptor.java
+++ src/main/java/org/saurabh/BankAccountDecryptor.java
@@ -5,20 +5,19 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
-public class Decryptor {
+public class BankAccountDecryptor {
 
     public static String decryptFrontendData(String encryptedData) throws Exception {
-        String key = "ASDFGHJKLASDFGHJ";
-        String iv = "cHr+mQdmOt7S9vtDwgYtdw==";
+        String decryptionKey = "ASDFGHJKLASDFGHJ";
+        String initializationVector = "cHr+mQdmOt7S9vtDwgYtdw==";
 
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "AES");
+        SecretKeySpec secretKeySpec = new SecretKeySpec(decryptionKey.getBytes(), "AES");
         Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(Base64.getDecoder().decode(iv)));
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(Base64.getDecoder().decode(initializationVector)));
 
         byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
         return new String(decryptedBytes).trim(); // trim to remove leading and trailing whitespaces
     }
-
 
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount("0x01", 14, 66,
@@ -27,16 +26,12 @@ public class Decryptor {
 
         try {
             System.out.println("Decrypted Bank Account Details:");
-            System.out.println("Account Holder Name: " + Decryptor.decryptFrontendData(bankAccount.getAccountHolderName()));
-            System.out.println("Account Number: " + Decryptor.decryptFrontendData(bankAccount.getAccountNumber()));
-            System.out.println("Bank Name: " + Decryptor.decryptFrontendData(bankAccount.getBankName()));
-            System.out.println("IFSC Code: " + Decryptor.decryptFrontendData(bankAccount.getIfscCode()));
+            System.out.println("Account Holder Name: " + decryptFrontendData(bankAccount.getAccountHolderName()));
+            System.out.println("Account Number: " + decryptFrontendData(bankAccount.getAccountNumber()));
+            System.out.println("Bank Name: " + decryptFrontendData(bankAccount.getBankName()));
+            System.out.println("IFSC Code: " + decryptFrontendData(bankAccount.getIfscCode()));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
-
-
-
